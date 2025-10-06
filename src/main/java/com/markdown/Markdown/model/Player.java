8b9e6abc7a75f9f0fe9876b4377de299072a440d
LATHAAJ <122860428+LATHAAJ@users.noreply.github.com>
2025-10-06 16:52:20 +0530

    first commit

---
+++ src/main/java/com/markdown/Markdown/model/Player.java
@@ -0,0 +1,4 @@
+package com.markdown.Markdown.model;
+
+public record Player(Integer id, String name, Team team, String city) {
+}
---
+++ src/main/java/com/markdown/Markdown/model/Team.java
@@ -0,0 +1,9 @@
+package com.markdown.Markdown.model;
+
+public enum Team {
+  CSK,
+  MI,
+  RCB,
+  DC,
+  GT
+}
